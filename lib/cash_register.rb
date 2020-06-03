487e9c70c31a7ad66ed660a6a98fa76abeccb963
Terren Culberson <culbersonterren@yahoo.com>
2020-06-03 03:06:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
 
+  attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, amount, quantity=1)
+     self.total += amount * quantity
+     quantity.times do
+       items << title
+  end
+  
+   def apply_discount
+  
+ end
+end
